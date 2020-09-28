116eeec42fc72b07a0c9e1706c80b344d17fa91c
Gaetan-R <70524141+Gaetan-R@users.noreply.github.com>
2020-09-29 00:19:25 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<form  action="index.php"  method="get">
+<?php
+
+$nameMovie = 'Indiana jones and the last crusade 🤠';
+echo $nameMovie;
+echo "<br />";
+
+$seeMovie = 'Did you watch the movie?';
+echo $seeMovie; 
+echo "<br />";
+
+$dateMovie ; 
+echo $dateMovie= "24 Mai 1989";
+echo "<br />";
+
+echo $noteMovie;
+echo $noteMovie =  8.2 
+
+?>
+      
+</form>
